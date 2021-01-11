3d4b5624ebd3e641448e368b7ad67fe3c120fdf1
keneishac <keneishacorbett@gmail.com>
2021-01-10 16:12:44 -0800

    fixes bigs

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -46,6 +46,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todos).permit(:item)
+      params.require(:todo).permit(:item)
     end
 end
