1c7a5a70849af55a92bec87c5308f9a34554951f
keneishac <keneishacorbett@gmail.com>
2021-01-11 11:17:12 -0800

    adds domain to CORS file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://morementum.herokuapp.com/todos/'
+    origins 'https://morementum-api.herokuapp.com/todos/'
 
     resource '*',
       headers: :any,
